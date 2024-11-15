0f4f73f85b5978c92e8b631310b874b81408c80e
S-al1 <salmaaitabbou972@gmail.comi>
2024-11-15 15:04:33 +0100

    final commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.getElementById('menu-button');
-    const collapsedMenu = document.getElementById('collapsed-menu');
-
-    menuButton.addEventListener('click', function () {
-        collapsedMenu.classList.toggle('hidden');
-    });
-});
