9b76df93f7fe3a19c722ab92079c79da4688cc34
sara boulahia <saraboulahia32@gmail.com>
2024-11-15 12:47:39 +0100

    commit before merge

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.getElementById('menu-button');
-    const collapsedMenu = document.getElementById('collapsed-menu');
-
+    const collapsedMenu = document.getElementById
     menuButton.addEventListener('click', function () {
         collapsedMenu.classList.toggle('hidden');
+    });
+    const searchButton = document.getElementById('search-button');
+    const collapsedsearch = document.getElementById('champRecherche');
+
+    searchButton.addEventListener('click', function () {
+        collapsedsearch.classList.remove('hidden');
     });
 });
